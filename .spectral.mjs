91703136d89ec20fe50a0f68f3781ca8cd23968e
ololadetemi <ololadetoluwalase@gmail.com>
2024-09-04 08:51:21 +0100

    updated

--- .spectral.mjs
+++
@@ -1,2 +0,0 @@
-import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjI1NzM3Mg/spectral.js?branch_slug=master&token=c42ec58c-a71d-46f9-b012-a627da789434";
-export default { extends: ruleset };
